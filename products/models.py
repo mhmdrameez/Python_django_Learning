99704832ca57582c361403c6fe094d71ce3590cf
muhammed rameez <31467986+mhmdrameez@users.noreply.github.com>
2022-03-04 21:31:52 +0530

    added models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+      name = models.CharField(max_length=255)
+      price = models.FloatField()
+      stock = models.IntegerField()
+      image = models.CharField(max_lenth=2500)
+      
